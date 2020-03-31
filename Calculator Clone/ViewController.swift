7832fe787fd8293c3651dfca9f6eb36421ed3f0c
John Nimis <john.nimis@gmail.com>
2020-03-31 09:01:04 -0500

    Initial Commit

---
+++ Calculator Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator Clone
+//
+//  Created by John Nimis on 3/31/20.
+//  Copyright © 2020 One Man Band Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
